720fbbfa48e2316dc50e34329c3e2f77e112fa68
NoaDevops <selaorinoaproject@gmail.com>
2020-05-26 18:29:10 +0300

    Adding CardView to activity_practice_select.xml file for categories

--- app/src/main/java/com/tomlezmy/goolmathapp/MainActivity.java
+++ app/src/main/java/com/tomlezmy/goolmathapp/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_learn);
+        setContentView(R.layout.activity_learn_select);
     }
 }
