53dfff9d57b8657cb7a8308ceb1df056974fc447
NoaDevops <selaorinoaproject@gmail.com>
2020-07-16 16:09:58 +0300

    Displaying sub categories in dialog while clicking on the first 4 categories, Save the category index and sub category index in ActivityLearnSelect activity

--- app/src/main/java/com/tomlezmy/goolmathapp/activities/LearnSelectActivity.java
+++ app/src/main/java/com/tomlezmy/goolmathapp/activities/LearnSelectActivity.java
@@ -1,15 +1,19 @@
 package com.tomlezmy.goolmathapp.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.tomlezmy.goolmathapp.R;
 import com.tomlezmy.goolmathapp.fragments.SubjectsFragment;
 
-public class LearnSelectActivity extends AppCompatActivity {
+public class LearnSelectActivity extends AppCompatActivity implements SubjectsFragment.OnSelectedSubCategoryListener {
 
     SubjectsFragment subjectsFragment;
+    int categorySelected;
+    int subCategorySelected;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,10 +22,19 @@ public class LearnSelectActivity extends AppCompatActivity {
         displaySubjectsForLearn();
     }
 
+    // Create SubjectFragment in LearnSelectActivity
     public void displaySubjectsForLearn() {
-        subjectsFragment = new SubjectsFragment(R.color.green_card);
+        int colorId = ContextCompat.getColor(this, R.color.green_card);
+        subjectsFragment = new SubjectsFragment(colorId);
         getSupportFragmentManager().beginTransaction()
                 .add(R.id.subjects_display_layout, subjectsFragment, "Subjects_TAG").commit();
     }
 
+
+    @Override
+    public void onSelectedSubCategory(int categoryId, int subCategoryId) {
+        Toast.makeText(this, "categoryId=" + " "  + categoryId + " " + "subCategory=" + subCategoryId, Toast.LENGTH_LONG).show();
+        this.categorySelected = categoryId;
+        this.subCategorySelected = subCategoryId;
+    }
 }
--- app/src/main/java/com/tomlezmy/goolmathapp/fragments/SubjectsFragment.java
+++ app/src/main/java/com/tomlezmy/goolmathapp/fragments/SubjectsFragment.java
@@ -1,16 +1,26 @@
 package com.tomlezmy.goolmathapp.fragments;
 
+import android.annotation.SuppressLint;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.cardview.widget.CardView;
+import androidx.core.content.ContextCompat;
+import androidx.core.content.res.ResourcesCompat;
 import androidx.fragment.app.Fragment;
-
 import com.tomlezmy.goolmathapp.R;
+
 
 public class SubjectsFragment extends Fragment {
 
@@ -23,22 +33,45 @@ public class SubjectsFragment extends Fragment {
     CardView fractionsCard;
     CardView percentagesCard;
     CardView decimal_numbersCard;
+    ArrayAdapter<String> adapter;
+    public interface OnSelectedSubCategoryListener {
+        void onSelectedSubCategory(int categoryId, int subCategoryId);
+    }
+
+    OnSelectedSubCategoryListener callback;
 
     // Constructor
     public SubjectsFragment(int cardsColor) {
         this.cardsColor = cardsColor;
-        // ... set cards color dynamically
+    }
+
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        try {
+            callback = (OnSelectedSubCategoryListener)context;
+        } catch (ClassCastException ex) {
+            throw new ClassCastException("The activity must implement on OnSelectedSubCategoryListener interface");
+        }
     }
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_subjects,container,false);
 
-        // References to all cards
+        /* References to all cards and set On click listener using private class
+            named CategoryCardsOnClickListener */
         additionCard = rootView.findViewById(R.id.card_addition_category);
+        additionCard.setOnClickListener(new CategoryCardsOnClickListener(0,getResources().getStringArray(R.array.additionSubCategories)));
         subtractionCard = rootView.findViewById(R.id.card_subtraction_category);
+        subtractionCard.setOnClickListener(new CategoryCardsOnClickListener(1,getResources().getStringArray(R.array.subtractionSubCategories)));
         multiplicationCard = rootView.findViewById(R.id.card_multiplication_category);
+        multiplicationCard.setOnClickListener(new CategoryCardsOnClickListener(2,getResources().getStringArray(R.array.multiplicationSubCategories)));
         divisionCard = rootView.findViewById(R.id.card_division_category);
+        divisionCard.setOnClickListener(new CategoryCardsOnClickListener(3,getResources().getStringArray(R.array.divisionSubCategories)));
+
+        // !!! I need to add the sub categories string array in strings.xml for those categories
         fractionsCard = rootView.findViewById(R.id.card_fractions_category);
         percentagesCard = rootView.findViewById(R.id.card_percentages_category);
         decimal_numbersCard = rootView.findViewById(R.id.card_decimal_numbers_category);
@@ -51,10 +84,49 @@ public class SubjectsFragment extends Fragment {
         fractionsCard.setCardBackgroundColor(this.cardsColor);
         percentagesCard.setCardBackgroundColor(this.cardsColor);
         decimal_numbersCard.setCardBackgroundColor(this.cardsColor);
-//        questionText.setText(question);
-//        questionText.setTextAppearance(R.style.FontLocalized);
-//        questionText.setTextSize(20f);
+
+
         return rootView;
+    }
+
+
+    // Set Card's Style and create list view to show all sub categories each time we click on a category
+    void displaySubCategories(final int categoryIndex, String[] subCategoriesArray) {
+        final AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+        TextView titleTv = new TextView(getContext());
+        titleTv.setText(R.string.pick_sub_category_txt);
+        titleTv.setTextSize(18f);
+        titleTv.setGravity(View.TEXT_ALIGNMENT_CENTER);
+        titleTv.setPadding(20,10,20,10);
+        titleTv.setTextColor(this.cardsColor);
+        Typeface typeface = ResourcesCompat.getFont(getContext(),R.font.patua_one);
+        titleTv.setTypeface(typeface, Typeface.NORMAL);
+        builder.setCustomTitle(titleTv);
+        adapter = new ArrayAdapter<String>(getContext(), R.layout.sub_category_list_view, subCategoriesArray);
+        builder.setAdapter(adapter,  new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                // Send back to the relevant activity,
+                callback.onSelectedSubCategory(categoryIndex, which);
+                dialog.dismiss();
+            }
+        }).show();
+    }
+
+
+    // Private class for handling cards on click listener
+    private class CategoryCardsOnClickListener implements View.OnClickListener {
+        String[] subCategoriesArray;
+        int categoryIndex;
+        // constructor
+        CategoryCardsOnClickListener(int categoryIndex, String[] subCategoriesArray) {
+            this.categoryIndex = categoryIndex;
+            this.subCategoriesArray = subCategoriesArray;
+        }
+        @Override
+        public void onClick(View v) {
+            displaySubCategories(this.categoryIndex,this.subCategoriesArray);
+        }
     }
 
 
