53f60a3557da6771768b180e78d3dec6f4410235
NoaDevops <selaorinoaproject@gmail.com>
2020-07-24 09:52:22 +0300

    Update MainActivity

--- app/src/main/java/com/tomlezmy/goolmathapp/activities/MainActivity.java
+++ app/src/main/java/com/tomlezmy/goolmathapp/activities/MainActivity.java
@@ -2,15 +2,48 @@ package com.tomlezmy.goolmathapp.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.tomlezmy.goolmathapp.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btnPractice;
+    Button btnLearn;
+    Button btnProgress;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_learn_select);
+        setContentView(R.layout.activity_main);
+
+
+        // references
+        btnPractice = findViewById(R.id.btn_practice);
+        btnPractice.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, PracticeSelectActivity.class);
+                startActivity(intent);
+            }
+        });
+        btnLearn = findViewById(R.id.btn_learn);
+        btnLearn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, LearnSelectActivity.class);
+                startActivity(intent);
+            }
+        });
+        btnProgress = findViewById(R.id.btn_progress);
+
+
     }
+
+
+
 }
