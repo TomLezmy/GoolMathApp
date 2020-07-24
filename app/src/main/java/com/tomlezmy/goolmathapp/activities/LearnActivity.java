5c9299f287a72f6033a07fe2cbfddd494c44c802
NoaDevops <selaorinoaproject@gmail.com>
2020-07-24 09:49:58 +0300

    Added Learn Activity

---
+++ app/src/main/java/com/tomlezmy/goolmathapp/activities/LearnActivity.java
@@ -0,0 +1,17 @@
+package com.tomlezmy.goolmathapp.activities;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.tomlezmy.goolmathapp.R;
+
+public class LearnActivity extends AppCompatActivity {
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_learn);
+        }
+}
+
+
