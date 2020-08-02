aa7940994f12eacaffe655d17d532331b00afd17
NoaDevops <selaorinoaproject@gmail.com>
2020-08-02 14:12:51 +0300

    Design the title of NumberPickerFragmentDialog

--- app/src/main/java/com/tomlezmy/goolmathapp/fragments/NumberPickerFragmentDialog.java
+++ app/src/main/java/com/tomlezmy/goolmathapp/fragments/NumberPickerFragmentDialog.java
@@ -5,27 +5,42 @@ import android.app.Dialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.widget.NumberPicker;
+import android.widget.TextView;
 
+import androidx.core.content.ContextCompat;
+import androidx.core.content.res.ResourcesCompat;
 import androidx.fragment.app.DialogFragment;
 
 import com.tomlezmy.goolmathapp.R;
 
 import java.util.Calendar;
+import java.util.Locale;
 
 public class NumberPickerFragmentDialog extends DialogFragment {
     private NumberPicker.OnValueChangeListener valueChangeListener;
 
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
-
         final NumberPicker numberPicker = new NumberPicker(getActivity());
 
         numberPicker.setMinValue(Calendar.getInstance().get(Calendar.YEAR) - 50);
         numberPicker.setMaxValue(Calendar.getInstance().get(Calendar.YEAR));
 
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-        builder.setMessage(getContext().getString(R.string.set_birth_year));
 
+        TextView tvSetBirthDay = new TextView(getContext());
+        tvSetBirthDay.setTextColor(ContextCompat.getColor(getContext(), R.color.colorMainHeaderPurple));
+        tvSetBirthDay.setPadding(20,20,20,20);
+        tvSetBirthDay.setText(R.string.set_birth_year);
+        if (Locale.getDefault().getDisplayLanguage().equalsIgnoreCase("English")) {
+            tvSetBirthDay.setTypeface(ResourcesCompat.getFont(getContext(), R.font.shortstack_regular));
+        }
+        else {
+            tvSetBirthDay.setTypeface(ResourcesCompat.getFont(getContext(), R.font.ganclm));
+        }
+        builder.setCustomTitle(tvSetBirthDay);
+//        builder.setMessage(getContext().getString(R.string.set_birth_year));
+        
         builder.setPositiveButton(getString(android.R.string.ok), new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
