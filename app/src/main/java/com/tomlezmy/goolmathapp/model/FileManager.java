50a4b39a38f60938f894bb0cc7e67194d8c34272
TomLezmy <49162863+TomLezmy@users.noreply.github.com>
2020-09-03 18:51:39 +0300

    Added javadoc to FileManager.java

--- app/src/main/java/com/tomlezmy/goolmathapp/model/FileManager.java
+++ app/src/main/java/com/tomlezmy/goolmathapp/model/FileManager.java
@@ -18,13 +18,24 @@ import java.util.Dictionary;
 import java.util.Hashtable;
 import java.util.List;
 
+/**
+ * This class manages all file access for user data and level weights
+ */
 public class FileManager {
+    /**
+     * Singleton instance
+     */
     private static FileManager instance;
     private Dictionary<ECategory,List<List<Integer>>> levelWeights;
     private UserData userData;
     private File levelWeightsFile;
     private File userDataFile;
 
+    /**
+     * Private constructor, called by {@link #getInstance} on first creation<br/>
+     * The constructor creates pointers to the userData and levelWeights files if they exist. if they don't then new files are created
+     * @param context The current context
+     */
     private FileManager(Context context) {
         // Read levelProbabilityWeights from file or create new file if doesn't exist
         levelWeights = new Hashtable<>();
@@ -47,10 +58,20 @@ public class FileManager {
         }
     }
 
+    /**
+     * This method checks if a user data file exists
+     * @param context The current context
+     * @return True if user data file exists
+     */
     public static boolean userFileExists(Context context) {
         return new File(context.getFilesDir().getAbsolutePath(),"user_data").exists();
     }
 
+    /**
+     * Singleton design pattern implementation
+     * @param context The current context
+     * @return The {@link FileManager} instance
+     */
     public static FileManager getInstance(Context context) {
         if (instance == null) {
             instance = new FileManager(context);
@@ -58,6 +79,11 @@ public class FileManager {
         return instance;
     }
 
+    /**
+     * This method reads the data from the levelWeights file and stores it in a {@link Dictionary}
+     * of type <{@link ECategory},{@link List}<{@link List}><{@link Integer}>>.<br/>
+     * Each game category is a key and the value is a list of levels where each level is divided into a list of sub-levels that hold the weight of each sub level
+     */
     private void readFromWeightsFile() {
         try {
             FileInputStream fis = new FileInputStream(levelWeightsFile);
@@ -73,6 +99,10 @@ public class FileManager {
         }
     }
 
+    /**
+     * This method reads the data from the userData file and stores it in a {@link Dictionary}
+     * of type <{@link UserData}
+     */
     private void readFromUserDataFile() {
         try {
             FileInputStream fis = new FileInputStream(userDataFile);
@@ -88,6 +118,9 @@ public class FileManager {
         }
     }
 
+    /**
+     * This method overrides the levelWeights file with the content of {@link #levelWeights}
+     */
     private void writeToWeightsFile() {
         try {
             FileOutputStream fos = new FileOutputStream(levelWeightsFile,false);
@@ -101,6 +134,9 @@ public class FileManager {
         }
     }
 
+    /**
+     * This method overrides the user data file with the content of {@link #userData}
+     */
     public void updateUserDataFile() {
         try {
             FileOutputStream fos = new FileOutputStream(userDataFile,false);
@@ -114,11 +150,24 @@ public class FileManager {
         }
     }
 
+    /**
+     * This method updates a specific sub level weight and then writes it to the levelWeights file
+     * @param category The current categroy
+     * @param level The current level
+     * @param subLevel The current sub level
+     * @param newValue The new weight value
+     */
     public void updateSubLevelWeight(ECategory category, int level, int subLevel, int newValue) {
         levelWeights.get(category).get(level).set(subLevel, newValue);
         writeToWeightsFile();
     }
 
+    /**
+     * @param category The required category
+     * @param level The required level
+     * @param subLevel The required sub level
+     * @return The sub level weight
+     */
     public int getSubLevelWeight(ECategory category, int level, int subLevel) {
         return levelWeights.get(category).get(level).get(subLevel);
     }
@@ -127,6 +176,12 @@ public class FileManager {
 
     public UserData getUserData() { return userData; }
 
+    /**
+     * This method creates a new user data file containing the user information and the level progress.<br/>
+     * The method will open specific levels if the user is older than 10
+     * @param firstName The user's name
+     * @param birthYear The user's birth year
+     */
     public void createNewUserDataFile(String firstName, int birthYear) {
         boolean userOverTen = (Calendar.getInstance().get(Calendar.YEAR) - birthYear > 10);
         Dictionary<ECategory, List<CategoryProgressData>> levelProgressData = new Hashtable<>();
@@ -141,6 +196,9 @@ public class FileManager {
         updateUserDataFile();
     }
 
+    /**
+     * This method creates a new weight file where all the sub levels have an equal weight of 1 for each category and level
+     */
     public void createNewWeightsFile() {
         levelWeights = new Hashtable<>();
         levelWeights.put(ECategory.ADDITION,Arrays.asList(Arrays.asList(1,1,1), Arrays.asList(1,1,1,1,1,1,1,1,1,1)));
